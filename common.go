f0986975551ed766e806cc136f883b2acdd100b9
George Pantazis <george.pantazis@vodafoneinnovus.com>
2023-04-15 17:52:13 +0300

    added a checfor perms just o reduce the lins in each cmd

--- common.go
+++ common.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func isPrintable(b byte) bool {
 	return b >= 32 && b <= 126
+}
+
+// Exit if we don't have permission to read the device
+func checkForPerms(deviceToRead string) {
+	if !hasReadPermission(deviceToRead) {
+		fmt.Printf("No permission to read the device: %s, try with elevated priviledges\n", deviceToRead)
+		os.Exit(13)
+	}
 }
